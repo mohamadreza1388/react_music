f4375dbc0f41cc54057983035eada518e8400a0d
mohamadreza <mohamadreza1388.org@gmail.com>
2024-11-27 00:09:04 +0330

    faet(router): add react router dom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+      "./index.html",
+      "./src/**/*.**",
+      "./src/**/*.jsx",
+  ],
   theme: {
     extend: {},
   },
